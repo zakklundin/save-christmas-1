7013a6e9d9189df533e6087fabb67361fcf811c7
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-23 15:02:30 +0100

    Uppgift 1

--- includes.js
+++ includes.js
@@ -1,9 +1,14 @@
-
 function includes(arr, item) {
     // for all i from 0 to arr.length - 1
     //      if arr[i] is equal to item, return true
 
     // no match was found, return false
+    for (let i = 0; i < arr.length ; i++) {
+        if (arr[i] === item){
+            return true
+        }
+    }
+    return false
 }
 
 const items = [
